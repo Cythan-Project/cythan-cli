a6deb8b00f1cdf5ae9bc9e2a619b1bbf6f7523bb
ccgauche <gaucheron.laurent@gmail.com>
2020-09-01 23:20:03 +0200

    Update to new cythan-compiler

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,6 +1,18 @@
 pub fn compile(file: &str, out: &str) -> anyhow::Result<()> {
-    let vec = cythan_compiler::Context::new().compute(&cythan_compiler::generate_tokens(
-        &std::fs::read_to_string(file)?,
-    ));
+    let vec = match cythan_compiler::Context::default().compute(
+        &match cythan_compiler::generate_tokens(&std::fs::read_to_string(file)?) {
+            Ok(e) => e,
+            Err(e) => {
+                println!("{}", e);
+                panic!("");
+            }
+        },
+    ) {
+        Ok(e) => e,
+        Err(e) => {
+            println!("{}", e);
+            panic!("");
+        }
+    };
     super::cythanenc::write_file_and_compress(out, &vec)
 }
